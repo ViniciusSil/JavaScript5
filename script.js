a9e0afa4018f041ce90a0e9ac91d9e1fb3ba1b7a
viniciusSilva <viniciusrs1560@gmail.com>
2023-10-30 16:06:02 -0300

    v1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Usando manipulação da DOM para adicionar um título ao site
+const titulo = document.getElementById('titulo');
+titulo.textContent = 'Produtos';
+
+// Usando manipulação da DOM para adicionar informações do produto
+const nomeProduto = document.getElementById('nomeProduto');
+nomeProduto.textContent = 'PC';
+
+const descricaoProduto = document.getElementById('descricaoProduto');
+descricaoProduto.textContent = 'produto ';
+
+const precoProduto = document.getElementById('precoProduto');
+precoProduto.textContent = ' R$ 99,99';
